0d7f4fe560950dd08403b4dfa6e726a978bad918
estivensal7 <estivensal7@gmail.com>
2020-06-30 00:56:57 -0400

    created the app's navbar component with reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 
+//Importing components
+import NavigationBar from "./Components/NavigationBar";
+
 function App() {
 	return (
 		<div className="App">
-			<h1>React App</h1>
+			<NavigationBar />
 		</div>
 	);
 }
---
+++ client/src/Components/NavigationBar/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import {
+	Collapse,
+	Navbar,
+	NavbarToggler,
+	NavbarBrand,
+	Nav,
+	NavItem,
+	NavLink,
+	NavbarText,
+} from "reactstrap";
+
+const NavigationBar = (props) => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => setIsOpen(!isOpen);
+
+	return (
+		<div>
+			<Navbar color="dark" dark expand="md">
+				<NavbarBrand href="/">Redux Shopping List</NavbarBrand>
+				<NavbarToggler onClick={toggle} />
+				<Collapse isOpen={isOpen} navbar>
+					<Nav className="ml-auto" navbar>
+						<NavItem>
+							<NavLink href="https://github.com/estivensal7/react-redux">
+								GitHub
+							</NavLink>
+						</NavItem>
+					</Nav>
+				</Collapse>
+			</Navbar>
+		</div>
+	);
+};
+
+export default NavigationBar;
