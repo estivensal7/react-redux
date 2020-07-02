6393dc5b08e48dcb8839a07dad11ceb9f507134d
estivensal7 <estivensal7@gmail.com>
2020-07-01 23:57:24 -0400

    added the ShoppingList component with json as the starter data in the state && added a CSSTransitionGroup for the add and delete options

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 
 //Importing components
 import NavigationBar from "./Components/NavigationBar";
+import ShoppingList from "./Components/ShoppingList";
 
 function App() {
 	return (
 		<div className="App">
 			<NavigationBar />
+			<ShoppingList />
 		</div>
 	);
 }
