fcb8a1c91e5250837247082d18ecad65343f9c8c
estivensal7 <estivensal7@gmail.com>
2020-07-02 10:48:18 -0400

    began setting up the react-redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./App.css";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 //Importing components
 import NavigationBar from "./Components/NavigationBar";
 import ShoppingList from "./Components/ShoppingList";
 
 function App() {
 	return (
-		<div className="App">
-			<NavigationBar />
-			<ShoppingList />
-		</div>
+		<Provider store={store}>
+			<div className="App">
+				<NavigationBar />
+				<ShoppingList />
+			</div>
+		</Provider>
 	);
 }
 
