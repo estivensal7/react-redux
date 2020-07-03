d74ca9ac852a0d5540ca4aad110e27923b103aa6
estivensal7 <estivensal7@gmail.com>
2020-07-03 03:47:54 -0400

    created the ItemModal component, and the DELETE_ITEM && ADD_ITEM actions

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,22 @@ import "./App.css";
 import { Provider } from "react-redux";
 import store from "./store";
 
+import { Container } from "reactstrap";
+
 //Importing components
 import NavigationBar from "./Components/NavigationBar";
 import ShoppingList from "./Components/ShoppingList";
+import ItemModal from "./Components//ItemModal";
 
 function App() {
 	return (
 		<Provider store={store}>
 			<div className="App">
 				<NavigationBar />
-				<ShoppingList />
+				<Container>
+					<ItemModal />
+					<ShoppingList />
+				</Container>
 			</div>
 		</Provider>
 	);
