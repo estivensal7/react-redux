18f1a0abd1b897c36cfa269800926619cc2ab431
estivensal7 <estivensal7@gmail.com>
2020-07-03 04:46:01 -0400

    updating routes index.js

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,4 @@ const apiRoutes = require("./api");
 
 router.use("/api", apiRoutes);
 
-router.use((req, res) => {
-	res.json({
-		message: "Where are you? 404 Bro",
-	});
-});
-
 module.exports = router;
