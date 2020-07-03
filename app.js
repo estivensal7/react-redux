83ff5bfcd09a72418dc68b9ccf0773a48ce86c05
estivensal7 <estivensal7@gmail.com>
2020-07-03 04:35:23 -0400

    prepared for deployment to heroku

--- app.js
+++ app.js
@@ -32,14 +32,14 @@ mongoose
 // Use Routes
 app.use(routes);
 
-// // Serve static assets if in production
-// if (process.env.NODE_ENV === "production") {
-// 	// Set static folder
-// 	app.use(express.static("client/build"));
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
 
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// 	});
-// }
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 module.exports = app;
